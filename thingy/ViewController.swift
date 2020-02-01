2751bc88a74ee5f223d6378f1ec98ca4a208f3ce
Ryo <ryoouchi0315@gmail.com>
2020-02-01 19:03:01 +0900

    Initial Commit

---
+++ thingy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  thingy
+//
+//  Created by Ryo on 2020/02/01.
+//  Copyright © 2020 Ryo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
